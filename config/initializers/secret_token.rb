8bee0a23359a2cb8788381b7752f9c1254f8053a
murilo_manhaes <murilo_manhaes@hotmail.com>
2012-07-03 21:03:47 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuriloSite::Application.config.secret_token = 'c0cb1911746a9cfe4b5f968f579086a05da952a3a25173f1120864604ca3ea4e9f3ab3f54948b03c47f84b52409e8b94b37c6fd834330cdb2faefa739a30ef8d'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+MuriloSite::Application.routes.draw do
+  root to: "home#index"
+
+end
